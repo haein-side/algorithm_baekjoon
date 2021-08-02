b8484abf8ed5573e9f6e11305f409d2ee68af599
Haein-bite <75101582+Haein-bite@users.noreply.github.com>
2021-08-02 17:25:17 +0900

    210802_baekjoon_algorithm_basic

---
+++ src/august_algorithm_level1_01/August02.java
@@ -0,0 +1,87 @@
+package august_algorithm_level1_01;
+
+import java.util.Scanner;
+
+public class August02  {
+
+	public static void main(String[] args) {
+		/* 백준 10718 */
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+		
+		/* 백준 10171 */
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+		/* 백준 10172 */
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+		
+		/* 백준 1000 */
+		Scanner sc = new Scanner(System.in);
+		
+		int A;
+		int B;
+		
+		//System.out.println("첫번째 숫자를 입력하세요");
+		A = sc.nextInt();
+		
+		//System.out.println("두번째 숫자를 입력하세요");
+		B = sc.nextInt();
+		
+		int C = A + B;
+		
+		System.out.println(C);
+		
+	
+		/* 백준 1001 */
+		Scanner sc2 = new Scanner(System.in);
+		
+		int d;
+		int e;
+		
+		d = sc2.nextInt();
+		e = sc2.nextInt();
+		
+		int f = d - e;
+		
+		System.out.println(f);
+		
+		/* 백준 1003 */
+		Scanner sc3 = new Scanner(System.in);
+		
+		int g;
+		int h;
+		
+		g = sc3.nextInt();
+		h = sc3.nextInt();
+		
+		int i = g * h;
+		
+		System.out.println(i);
+		
+		/* 백준 1008 */
+		Scanner sc4 = new Scanner(System.in);
+		
+		double j;
+		double k;
+		
+		j = sc4.nextDouble();
+		k = sc4.nextDouble();
+		
+		double l = j / k;
+		//double m = j % k;
+		
+		System.out.println(l);
+		//System.out.println(m);
+		
+		System.out.println(l);
+		
+	} 
+	
+}
