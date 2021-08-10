cc83432ecaaa784a92824a125ea5304f4e7ba0dc
Haein-bite <75101582+Haein-bite@users.noreply.github.com>
2021-08-10 21:27:18 +0900

    210810 - 백준 for문 문풀 중

---
+++ src/august_algorithm_level1_01/August10.java
@@ -0,0 +1,29 @@
+package august_algorithm_level1_01;
+
+import java.util.Scanner;
+
+public class August10 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int roop = sc.nextInt();
+		int[] sum = new int[roop];
+		
+		for (int i = 0 ; i < roop; i++) {
+			
+			int num1 = sc.nextInt();
+			int num2 = sc.nextInt();
+			
+			sum[i] += (num1 + num2);
+		}
+		
+		
+		for (int j = 0; j < roop; j++) {
+			System.out.println(sum[j]);
+		}
+		
+	}
+
+}
