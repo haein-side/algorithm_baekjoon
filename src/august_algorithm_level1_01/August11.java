be09a16e32e677bc7d65c2f8bba0f8ecc28ca26e
Haein-bite <75101582+Haein-bite@users.noreply.github.com>
2021-08-12 00:01:27 +0900

    210812 - 백준 for문

--- src/august_algorithm_level1_01/August11.java
+++ src/august_algorithm_level1_01/August11.java
@@ -6,6 +6,8 @@ public class August11 {
 
 	public static void main(String[] args) {
 		
+		/* 백준 2741 */
+		
 		Scanner sc = new Scanner(System.in);
 		
 		int roop = sc.nextInt();
@@ -13,6 +15,17 @@ public class August11 {
 		for (int i = 0; i < roop; i++) {
 			System.out.println(i+1);
 		}
+		
+		/* 백준 2742 */
+		
+		Scanner sc2 = new Scanner(System.in);
+		
+		int roop2 = sc2.nextInt();
+		
+		for(int j = 0; j < roop2; j++) {
+			System.out.println(roop2-j);
+		}
+		
 		
 	}
 
