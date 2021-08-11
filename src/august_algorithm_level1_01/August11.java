9072191466d854253cc7f4a0aab89304b4586b01
Haein-bite <75101582+Haein-bite@users.noreply.github.com>
2021-08-11 23:55:01 +0900

    210811 - 백준 for문

---
+++ src/august_algorithm_level1_01/August11.java
@@ -0,0 +1,19 @@
+package august_algorithm_level1_01;
+
+import java.util.Scanner;
+
+public class August11 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int roop = sc.nextInt();
+		
+		for (int i = 0; i < roop; i++) {
+			System.out.println(i+1);
+		}
+		
+	}
+
+}
