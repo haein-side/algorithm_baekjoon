a724ed4c6dc9b86267d56b4f5ad5de8c7d6c30c1
haein-side <75101582+haein-side@users.noreply.github.com>
2021-08-15 23:31:11 +0900

    Create August15.java

---
+++ src/august_algorithm_level1_01/August15.java
@@ -0,0 +1,26 @@
+package august_algorithm_level1_01;
+
+import java.util.Scanner;
+
+public class August15 {
+
+	public static void main(String[] args) {
+
+		/* 백준 10952 while문 */
+		
+		while(true) {
+			
+			Scanner sc = new Scanner(System.in);
+			
+			int num1 = sc.nextInt();
+			int num2 = sc.nextInt();
+			
+			int sum = num1 + num2;
+			
+			System.out.println(sum);
+			
+		}
+		
+	}
+
+}
