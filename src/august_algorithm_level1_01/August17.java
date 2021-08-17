16752688fbf63e47204bc22efa6335b2ff10a8e4
haein-side <75101582+haein-side@users.noreply.github.com>
2021-08-17 14:37:58 +0900

    백준 while 문 완료

---
+++ src/august_algorithm_level1_01/August17.java
@@ -0,0 +1,45 @@
+package august_algorithm_level1_01;
+
+import java.util.Scanner;
+
+//import java.util.Scanner;
+
+public class August17 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int numOrigin = sc.nextInt();
+		int numComp = numOrigin;
+		
+		int num1 = 0;
+		int num2 = 0;
+		
+		int sum = 0;
+		
+		int roop = 0;
+		
+		while(true) {
+			
+			num2 = numOrigin % 10;
+			num1 = numOrigin / 10;
+			sum = num1 + num2;
+			
+			int newNum = num2 * 10 + sum % 10;
+			roop += 1;
+			
+			System.out.println("newNum : " + newNum);
+			
+			if(newNum == numComp) {
+				break;
+			}
+			
+			numOrigin = newNum;
+			
+		}
+		
+		System.out.println("사이클 : " + roop);
+		
+	}
+}
