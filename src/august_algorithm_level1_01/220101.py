6e7ffc676ac869dd0a0703bef60e2983a74a0575
Haein-bite <75101582+Haein-bite@users.noreply.github.com>
2022-01-02 13:01:24 +0900

    boj1157 풀이

--- src/august_algorithm_level1_01/220101.py
+++ src/august_algorithm_level1_01/220101.py
@@ -1,97 +1,97 @@
-# area = int(input("직사각형 넓이를 입력 :"))
+area = int(input("직사각형 넓이를 입력 :"))
 
-# for i in range(1, area + 1) :
-#     if i * i >  area : break
-#     if area % i != 0 : continue 
-#     print(f'{i} x {area // i}')
+for i in range(1, area + 1) :
+    if i * i >  area : break
+    if area % i != 0 : continue 
+    print(f'{i} x {area // i}')
     
     
-# # 실습 1-19
-# word = ""
+# 실습 1-19
+word = ""
 
-# for i in range(1, 13)  :
-#     if i != 8 :
-#         word.append(str(i) + " ")
+for i in range(1, 13)  :
+    if i != 8 :
+        word.append(str(i) + " ")
 
-# print(word)
+print(word)
 
-# for i in range(1, 13):
-#     if i == 8 :
-#         continue
-#     print(i, end=" ")
-# print()
+for i in range(1, 13):
+    if i == 8 :
+        continue
+    print(i, end=" ")
+print()
 
-# for i in list(range(1,8)) + list(range(9,13)):
-#     print(i, end= " ")
+for i in list(range(1,8)) + list(range(9,13)):
+    print(i, end= " ")
     
-# # 실습 1C-3
-# print('2자리 양수 입력하세요')
+# 실습 1C-3
+print('2자리 양수 입력하세요')
 
-# while True :
-#     no = int(input("값을 입력하세요 :"))
-#     if no >= 10 and no <= 99 :
-#         print(f'입력받은 정수는 {no}입니다')
-#         break
+while True :
+    no = int(input("값을 입력하세요 :"))
+    if no >= 10 and no <= 99 :
+        print(f'입력받은 정수는 {no}입니다')
+        break
 
-# # 실습 1-21
-# print("-"*27)
+# 실습 1-21
+print("-"*27)
 
-# for i in range(1,10) :
-#     for j in range(1,10):
-#         print(i*j, end = " ")
-#     print()
+for i in range(1,10) :
+    for j in range(1,10):
+        print(i*j, end = " ")
+    print()
 
-# # 실습 1-22
-# n = int(input("짧은 변의 길이를 입력하세요 : "))
+# 실습 1-22
+n = int(input("짧은 변의 길이를 입력하세요 : "))
 
-# for i in range(n):
-#     print("*"*(i+1))
+for i in range(n):
+    print("*"*(i+1))
     
-# for j in range(n) :
-#     for k in range(j+1) :
-#         print("*",end = "")
-#     print()
+for j in range(n) :
+    for k in range(j+1) :
+        print("*",end = "")
+    print()
 
-# # 실습 1-23
-# n = int(input('짧은 변의 길이를 입력하라 : '))
+# 실습 1-23
+n = int(input('짧은 변의 길이를 입력하라 : '))
 
-# for i in range(n):
-#     for j in range(n-(i+1)):
-#         print(" ",end="")
-#     for k in range(i+1):
-#         print("*",end="")
-#     print()
+for i in range(n):
+    for j in range(n-(i+1)):
+        print(" ",end="")
+    for k in range(i+1):
+        print("*",end="")
+    print()
 
-# # 실습 2-1
-# sum = 0
+# 실습 2-1
+sum = 0
 
-# for i in range(5) : #0,1,2,3,4 가 i로 들어감
-#     n = int(input(f'{i+1}번 학생의 점수를 입력하세요:'))
-#     sum += n
-# print(f'합계는 {sum}입니다.')
-# print(f'평균은 {sum/5}입니다.')
+for i in range(5) : #0,1,2,3,4 가 i로 들어감
+    n = int(input(f'{i+1}번 학생의 점수를 입력하세요:'))
+    sum += n
+print(f'합계는 {sum}입니다.')
+print(f'평균은 {sum/5}입니다.')
 
-# list05 = list('ABC')
-# list06 = list([1,2,3])
-# list07 = list((1,2,3))
-# list08 = list({1,2,3})
-# list09 = list(range(5))
-# list10 = list(range(3,8))
-# list11 = list(range(3,9,2))
-# print(list05)
-# print(list06)
-# print(list07)
-# print(list08)
-# print(list09)
-# print(list10)
-# print(list11)
+list05 = list('ABC')
+list06 = list([1,2,3])
+list07 = list((1,2,3))
+list08 = list({1,2,3})
+list09 = list(range(5))
+list10 = list(range(3,8))
+list11 = list(range(3,9,2))
+print(list05)
+print(list06)
+print(list07)
+print(list08)
+print(list09)
+print(list10)
+print(list11)
 
-## 리스트의 원소의 개수는 반드시 리스트를 만들기 전에 결정해야 함
-## 그러나 리스트의 원소값은 None으로 정해두지 않을 수도 있음!
+# 리스트의 원소의 개수는 반드시 리스트를 만들기 전에 결정해야 함
+# 그러나 리스트의 원소값은 None으로 정해두지 않을 수도 있음!
 
-# list12 = [None] * 5
+list12 = [None] * 5
 
-# print(list12)
+print(list12)
 
 
 x = [11,22,33,44,55,66,77]
--- src/august_algorithm_level1_01/boj1157.py
+++ src/august_algorithm_level1_01/boj1157.py
@@ -1,21 +1,77 @@
-sen = input()
+# sen = input()
+
+# word = [0]*len(sen)
 
 
-word = [0]*len(sen)
+# for i in sen :
+#   for j in range(len(sen)) :
+#     if i == sen[j] or (max(ord(sen[j]),ord(i)) - min(ord(sen[j]),ord(i)) == 32):
+#       # word[i] += 1
+#       # print(i)
+#       word[sen.index(i)] = word[sen.index(i)] + 1
+
+# for i in range(len(word)) :
+#   num =0
+#   if max(word) == word[i] :
+#     num +=1
+#   if num >= 2 :
+#     print("?")
+#   else :
+#     print(sen[word.index(max(word))])
 
 
-for i in sen :
-  for j in range(len(sen)) :
-    if i == sen[j] or (max(ord(sen[j]),ord(i)) - min(ord(sen[j]),ord(i)) == 32):
-      # word[i] += 1
-      # print(i)
-      word[sen.index(i)] = word[sen.index(i)] + 1
+# # 두 번째 시도
+# word = input()
+# list = list(word)
 
-for i in range(len(word)) :
-  num =0
-  if max(word) == word[i] :
-    num +=1
-  if num >= 2 :
+# result = ""
+# num = 0
+
+# while True :
+#   sum = 0
+#   alpha = list[0]
+  
+#   sum += list.count(list[0])
+#   sum += list.count(list[0].upper())
+  
+#   a = list.count(list[0])
+#   b = list.count(list[0].upper())
+  
+#   if alpha.lower() in list :
+#     for _ in range(a) :
+#         list.remove(list[0])
+        
+#   if alpha.upper() in list :
+#     for _ in range(b) :
+#         list.remove(list[0].upper())
+      
+#   # print(sum)
+#   # print(list)
+  
+#   if sum > num :
+#     result = alpha
+#     num = sum
+#   if sum == num :
+#     result = '?'
+    
+#   if len(list) == 0 :
+#     break
+  
+# print(result.upper())
+
+
+
+# 1157번 : 단어 공부
+word = input().lower()      # word = mississipi / baaa
+word_list = list(set(word)) # word_list = ['p', 'm', 'i', 's'] / ['b', 'a']
+print(word_list)
+
+cnt = []
+
+for i in word_list:         # i = p, m, i, s / b, a
+    count = word.count(i)
+    cnt.append(count)       # cnt = [1, 1, 4, 4] / [1, 3]
+if cnt.count(max(cnt)) >= 2: # max인 것의 갯수가 2개 이상이면 ? 출력
     print("?")
-  else :
-    print(sen[word.index(max(word))])
+else:
+    print(word_list[(cnt.index(max(cnt)))].upper())
