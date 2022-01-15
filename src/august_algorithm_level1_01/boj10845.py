8f0051e5374d7254142c57919b44b0456c44bcaf
Haein-bite <75101582+Haein-bite@users.noreply.github.com>
2022-01-15 23:58:41 +0900

    220115 - 릿코드

--- src/august_algorithm_level1_01/boj10845.py
+++ src/august_algorithm_level1_01/boj10845.py
@@ -1,7 +1,7 @@
-import sys
-N = int(sys.stdin.readline())
+# import sys
+# N = int(sys.stdin.readline())
 
-for i in range(N):
-    order = sys.stdin.readline().split()
+# for i in range(N):
+#     order = sys.stdin.readline().split()
     
-    if order[0]
+#     if order[0] :
