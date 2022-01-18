48dd3da02b403f04ea40248dfdda74fb60a4324e
Haein-bite <75101582+Haein-bite@users.noreply.github.com>
2022-01-18 22:47:50 +0900

    220118 - 백준 큐 풀이

--- src/august_algorithm_level1_01/boj10845.py
+++ src/august_algorithm_level1_01/boj10845.py
@@ -1,7 +1,33 @@
-# import sys
-# N = int(sys.stdin.readline())
+import sys
 
-# for i in range(N):
-#     order = sys.stdin.readline().split()
+n = int(sys.stdin.readline())
+que = []
     
-#     if order[0] :
+for i in range(n):
+    order = sys.stdin.readline().split()
+    
+    if order[0] == "push":
+        que.append(order[1])
+    elif order[0] == "pop":
+        if len(que) > 0:
+            print(que[0])
+            que.remove(que[0])
+        else :
+            print("-1")
+    elif order[0] == "size":
+        print(len(que))
+    elif order[0] == "empty":
+        if len(que) == 0:
+            print("1") 
+        else:
+            print("0")
+    elif order[0] == "front":
+        if len(que) > 0:
+            print(que[0])
+        else :
+            print("-1")
+    else :
+        if len(que) > 0:
+           print(que[-1])
+        else :
+           print("-1")
