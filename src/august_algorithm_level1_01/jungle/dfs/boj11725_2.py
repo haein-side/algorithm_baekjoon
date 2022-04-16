50bbd65f21546f5e829a4c55c333bb24cc941012
haein-side <kimhi1640@gmail.com>
2022-04-16 14:55:25 +0900

    220416 백준 문제 풀이

--- src/august_algorithm_level1_01/jungle/dfs/boj11725_2.py
+++ src/august_algorithm_level1_01/jungle/dfs/boj11725_2.py
@@ -1,34 +1,35 @@
 # 이 풀이는 따로 부모 테이블 같은 걸 두지 않고 풀었다.
 # 자식노드가 2개 이상일 때 성립하지 않는 코드..
-
 import sys
+sys.setrecursionlimit(10**6)
+
 N = int(sys.stdin.readline())
 node = [[] for i in range(N+1)] # 0번째 노드는 비워두기
-
+one = []
 for i in range(N-1):
     a, b = map(int, sys.stdin.readline().split())
     node[a].append(b)
     node[b].append(a) # 2차원 행렬 이외에 리스트로 받는 방안 or 딕셔너리로 받는 방안도 구현해보기
 
 visited = [False] * (N+1)
-trace = []
+dic = {} # dic[자식 노드] = 부모 노드 (부모 노드는 자식 노드별로 한 개!)
+
+for i in range(N):
+    dic[i] = 0
 
 def dfs(i):
-    global trace
-    
     visited[i] = True
-    trace.append(i)
     
-    for j in node[i]: # i 노드와 인접한 노드 j
+    for j in node[i]: # i 노드와 인접한 노드 j            
         if not visited[j]:
+            dic[j] = i # dic[자식 노드] = 부모 노드 | {0: 0, 1: 0, 2: 4, 3: 6, 4: 1, 5: 3, 6: 1, 7: 4}
             dfs(j)
     
 for i in range(1, N+1): # 1부터 N까지 노드
     if not visited[i]:
         dfs(i)
 
-for i in range(2, N+1):
-    if i not in node[1]:
-        print(trace[trace.index(i)-1])
-    else:
-        print(1)
+# print(dic)
+for i in dic.values():
+    if i > 0:
+        print(i)
---
+++ src/august_algorithm_level1_01/jungle/dfs/boj1707.py
@@ -0,0 +1,48 @@
+from collections import deque
+import sys
+
+T = int(sys.stdin.readline())
+
+for i in range(T):
+    V, E = map(int, sys.stdin.readline().split())  # V: 정점의 개수 | E: 간선의 개수
+    node = [[] for i in range(V+1)] # 0번째 노드는 비워두기
+    visited = [0] * (V+1)
+
+    for j in range(E):
+        a, b = map(int, sys.stdin.readline().split())
+        node[a].append(b)
+        node[b].append(a)
+    
+    def bfs(i):
+        queue = deque()
+        queue.append(i)
+        # 현재 노드 방문 처리
+        visited[i] = 1
+        # 큐가 빌 때까지 반복
+        while queue:
+            # 큐에서 원소 하나를 뽑아 출력
+            v = queue.popleft()
+            color = visited[v] # 인접 노드 보기 전 노드의 색깔을 color 변수에 담아줌 (그 전에 색칠했던 게 무슨 색인지 아는 법)
+            for k in node[v]:
+                if visited[k] == 0 : # 인접 노드를 방문한 적이 없을 경우
+                    queue.append(k)
+                    visited[k] = -color # 인접 노드 보기 전 노드의 색깔과 반대로 줌 (이분 그래프는 인접한 정점이 같은 색이면 안 되므로)
+                elif visited[k] == 1: # 인접노드에 방문한 적이 있을 경우, 색깔이 1일 때
+                    if color == visited[k]:
+                        return False
+                elif visited[k] == -1:
+                    if color == visited[k]:
+                        return False
+        
+        return True
+    
+    # bfs(1) # 시작노드를 1로 하고 한 번만 호출하면 되나? - 안됨!
+    # 노드 하나씩 bfs를 호출해야 함 <- 왜?
+    for e in range(1, V+1):
+        if not visited[e]: # 방문한 정점이 아니라면, bfs 수행
+            result = bfs(e) # 
+            if result == False:
+                print("NO")
+                break
+    else:
+        print("YES")
